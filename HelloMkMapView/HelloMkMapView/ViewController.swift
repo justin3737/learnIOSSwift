9eddc68a70a5943010671748d92709fc8130ec27
Justin <juns3700@gmail.com>
2016-01-07 22:45:23 +0800

    Map Add popup

--- HelloMkMapView/HelloMkMapView/ViewController.swift
+++ HelloMkMapView/HelloMkMapView/ViewController.swift
@@ -12,6 +12,16 @@ import MapKit
 class ViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var map: MKMapView!
+    @IBAction func addMeAnnoaton(sender: UILongPressGestureRecognizer) {
+        let touchPoint = sender.locationInView(self.map)
+        let touchCoordinate:CLLocationCoordinate2D =  map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = touchCoordinate
+        annotation.title = "New Place"
+        annotation.subtitle = "Oneday I wanna be here!"
+        map.addAnnotation(annotation)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         let latitude : CLLocationDegrees = 48.858547
@@ -22,8 +32,14 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let span     : MKCoordinateSpan = MKCoordinateSpanMake(yScale, xScale)
         let region   : MKCoordinateRegion = MKCoordinateRegionMake(location, span)
         map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Effiel Tower"
+        annotation.subtitle = "I was here once!"
+        map.addAnnotation(annotation)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -31,4 +47,3 @@ class ViewController: UIViewController, MKMapViewDelegate {
 
 
 }
-
